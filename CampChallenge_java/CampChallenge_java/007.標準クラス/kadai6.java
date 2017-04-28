16344e109a02e956ed99bb1c7b668095301e1982
VAIO\kenta <ke.n.ta.oqii@gmail.com>
2017-04-28 16:45:27 +0900

    標準クラス課題7提出します。

---
+++ CampChallenge_java/CampChallenge_java/007.標準クラス/kadai6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package date;
+
+/**
+ *
+ * @author kenta
+ */
+public class kadai6 {
+    //課題6 文字数の取得
+    //自分のメールアドレスの「@」以降の文字を取得して、表示してください。
+    public static void main(String[]args){
+        String add = "ke.n.ta.oqii@gmail.com";
+        System.out.println(add.substring(12));
+    }
+    
+}
