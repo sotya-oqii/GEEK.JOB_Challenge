ad588c423d90ff3d7d01776062aa2ef929559ee8
VAIO\kenta <ke.n.ta.oqii@gmail.com>
2017-04-28 16:44:55 +0900

    標準クラス課題5提出します。

---
+++ CampChallenge_java/CampChallenge_java/007.標準クラス/kadai5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package date;
+
+/**
+ *
+ * @author kenta
+ */
+    //課題5　バイト数と文字数の取得
+    //自分の氏名について、バイト数と文字数を取得して、表示してください。
+public class kadai5 {
+    public static void main(String[]args){
+        String moji = "井上健太";
+        System.out.println(moji.length());
+        System.out.println(moji.substring(2));          //1,3とかもできる
+        
+    }
+    
+}
